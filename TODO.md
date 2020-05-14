f37f6f4ec2585357503ad752292f331a61903e2a
Mikhail Alekseev <mikhail.alexeev.dmr@gmail.com>
2020-05-14 17:01:49 +0300

    Finish dirty Incentives UI

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
 * Add Goals & Incentives CRUD UI
 * Add Daily model
 * Add Daily UI
+* Icons
 * Optimize UI
 * Optimize mobile UI
 
--- app/controllers/web/v1/incentives_controller.rb
+++ app/controllers/web/v1/incentives_controller.rb
@@ -7,6 +7,10 @@ module Web
         @incentives = current_web_v1_user.incentives.order(size: :asc)
       end
 
+      def show
+        @incentive = current_web_v1_user.incentives.find(params[:id])
+      end
+
       def new
         @incentive ||= current_web_v1_user.incentives.build
       end
@@ -15,9 +19,38 @@ module Web
         @incentive = current_web_v1_user.incentives.create(params.require(:incentive).permit(:name, :size))
 
         if @incentive.persisted?
+          # TODO: flash OK
           redirect_to web_v1_incentives_path
         else
+          # TODO: flash ERROR
           render :new
+        end
+      end
+
+      def edit
+        @incentive ||= current_web_v1_user.incentives.find(params[:id])
+      end
+
+      def update
+        @incentive = current_web_v1_user.incentives.find(params[:id])
+
+        if @incentive.update(params.require(:incentive).permit(:name, :size))
+          # TODO: flash OK
+          redirect_to web_v1_incentives_path
+        else
+          # TODO: flash ERROR
+          render :edit
+        end
+      end
+
+      def destroy
+        @incentive = current_web_v1_user.incentives.find(params[:id])
+
+        if @incentive.destroy
+          # TODO: flash OK
+          redirect_to web_v1_incentives_path
+        else
+          # TODO: flash ERROR
         end
       end
     end

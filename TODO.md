363234286c7f340a22c80a8c708026e53f9d2080
Mikhail Alekseev <mikhail.alexeev.dmr@gmail.com>
2020-05-26 12:14:25 +0300

    Enum metaprogramming

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
-* Metaprogramming for enums
 * Optimize UI (especially stats + statuses)
 * Optimize mobile UI
 * Save "daily generated" & "yesterday frozen" for user
 * README
 * API v2 + Vue.js
+* Daily analytics?
+* Week analytics?
+* Drop old dailies?
--- app/models/concerns/with_enum.rb
+++ app/models/concerns/with_enum.rb
@@ -1,21 +1,23 @@
 module WithEnum
   extend ActiveSupport::Concern
 
-  class_methods do
-    def enum_display(enum_attr, value)
-      human_attribute_name([enum_attr, value].join('/'))
-    end
+  included do |base|
+    base.class_eval do
+      base.attribute_types.select { |_, v| v.is_a?(ActiveRecord::Enum::EnumType) }.keys.each do |enum_attr|
+        define_singleton_method("display_#{enum_attr}") do |value|
+          base.human_attribute_name([enum_attr, value].join('/'))
+        end
 
-    def enum_options_for_select(enum_attr)
-      public_send(enum_attr.to_s.pluralize).map do |k, _|
-        [enum_display(enum_attr, k), k]
+        define_method("display_#{enum_attr}") do
+          self.class.public_send("display_#{enum_attr}", public_send(enum_attr))
+        end
+
+        define_singleton_method("#{enum_attr}_options_for_select") do
+          base.public_send(enum_attr.to_s.pluralize).map do |k, _|
+            [base.public_send("display_#{enum_attr}", k), k]
+          end
+        end
       end
-    end
-  end
-
-  included do
-    def enum_display(enum_attr)
-      self.class.enum_display(enum_attr, public_send(enum_attr))
     end
   end
 end
